6b7ebf736d5b9316970d8e987d114e77439b3a87
NiceNiceSwan <kubapawelczyk41@gmail.com>
2026-03-10 20:11:54 +0100

    adds documentation and a test routine, fixes bugs

--- src/stepper_controller.cpp
+++ src/stepper_controller.cpp
@@ -1,10 +1,15 @@
 #include "stepper_controller.h"
 
-int Stepper_controller::_clamp(int value, int min_, int max_)
+int Stepper_controller::_clamp(int value, const int &min_, const int &max_)
 {
     value = min(value, max_);
     value = max(value, min_);
     return value;
+}
+
+Stepper_controller::Stepper_controller(AccelStepper stepper)
+{
+    _stepper_motor = stepper;
 }
 
 Stepper_controller::Stepper_controller(int pin1, int pin2, int pin3, int pin4)
@@ -33,27 +38,50 @@ void Stepper_controller::attach(int pin1, int pin2, int pin3, int pin4)
 void Stepper_controller::move_to_position(int position)
 {
     _target_position = _logical_to_physical(position);
+    _stepper_motor.setSpeed(_target_position > 0 ? MOTOR_MAIN_SPEED : -MOTOR_MAIN_SPEED);
     _stepper_motor.moveTo(_target_position);
-    // _stepper_motor.setSpeed(_target_position > 0 ? MOTOR_MAIN_SPEED : -MOTOR_MAIN_SPEED);
-    // Serial.print("Position:");
-    // Serial.print(_logical_position());
-    // Serial.println(" steps");
 }
 
 void Stepper_controller::move_by_amount(int amount)
 {
-    _target_position = _stepper_motor.currentPosition() + amount;
+    _target_position = _stepper_motor.currentPosition() - amount;
+    _stepper_motor.setSpeed(_target_position > 0 ? MOTOR_MAIN_SPEED : -MOTOR_MAIN_SPEED);
     _stepper_motor.moveTo(_target_position);
-    // _stepper_motor.setSpeed(_target_position > 0 ? MOTOR_MAIN_SPEED : -MOTOR_MAIN_SPEED);
+}
+
+void Stepper_controller::set_origin()
+{
+    _origin_offset_steps = _stepper_motor.currentPosition();
+}
+
+void Stepper_controller::test_routine()
+{
+    Serial.print("Starting test routine\n");
+    Serial.print("Move to position: 500\n");
+    move_to_position(500);
+    priority_move();
+    Serial.print("Move to position: 0\n");
+    move_to_position(0);
+    priority_move();
+    Serial.print("Move by amount: 500\n");
+    move_by_amount(500);
+    priority_move();
+    Serial.print("Setting origin\n");
+    set_origin();
+    Serial.print("Move to position: 500\n");
+    move_to_position(500);
+    priority_move();
+    Serial.print("Move by amount: -1000\n");
+    move_by_amount(-1000);
+    priority_move();
+    Serial.print("Test routine finished\n");
 }
 
 long Stepper_controller::_logical_position() {
-  // Logical coordinates are inverted vs. motor direction and referenced to originOffsetSteps
     return _stepper_motor.currentPosition() - _origin_offset_steps;
 }
 
 long Stepper_controller::_logical_to_physical(long logicalSteps) {
-  // Convert desired logical position to physical steps for AccelStepper
     return _origin_offset_steps - logicalSteps;
 }
 
@@ -78,7 +106,7 @@ bool Stepper_controller::take_serial_input(String input)
     }
     if (input.startsWith(STEPPER_TEST_ROUTINE))
     {
-        // test_routine();
+        test_routine();
         return true;
     }
     return false;
@@ -90,8 +118,22 @@ void Stepper_controller::handle_movement()
     {
         Serial.print("current Position:");
         Serial.print(_stepper_motor.currentPosition());
-        digitalWrite(ENABLE_PIN_MAIN_MOTOR, LOW);
+        digitalWrite(ENABLE_PIN_STEPPER_MOTOR, LOW);
         _stepper_motor.runSpeedToPosition();
-        digitalWrite(ENABLE_PIN_MAIN_MOTOR, HIGH);
+        digitalWrite(ENABLE_PIN_STEPPER_MOTOR, HIGH);
+        _moving = true;
+        return;
+    }
+    _moving = false;
+}
+
+void Stepper_controller::priority_move()
+{
+    while (_stepper_motor.currentPosition() != _target_position)
+    {
+        digitalWrite(ENABLE_PIN_STEPPER_MOTOR, LOW);
+        _stepper_motor.runSpeedToPosition();
+        digitalWrite(ENABLE_PIN_STEPPER_MOTOR, HIGH);
     }
 }
+
--- src/stepper_controller.h
+++ src/stepper_controller.h
@@ -14,9 +14,7 @@
 #define HALFSTEP 2
 #define FULLSTEP 4
 
-#define DIR_PIN_MAIN_MOTOR 2
-#define STEP_PIN_MAIN_MOTOR 3
-#define ENABLE_PIN_MAIN_MOTOR 4
+#define ENABLE_PIN_STEPPER_MOTOR 4
 #define MOTOR_MAX_SPEED 10000
 #define MOTOR_ACCELERATION 10000
 #define MOTOR_MAIN_SPEED 10000
@@ -26,44 +24,59 @@
 class Stepper_controller
 {
 private:
-    // AccelStepper _stepper_motor(MOTOR_INTERFACE_TYPE, STEP_PIN_MAIN_MOTOR, DIR_PIN_MAIN_MOTOR);
+    /// @brief our stepper motor
     AccelStepper _stepper_motor;
     /// @brief clamps the value
     /// @param value value which you want clamped
     /// @param min minimum value this can be
     /// @param max maximum value this can be
     /// @return value clamped to range min and max, including min and max
-    int _clamp(int value, int min, int max);
+    int _clamp(int value, const int &min, const int &max);
 
     int _attachment_pin1;
     int _attachment_pin2;
     int _attachment_pin3;
     int _attachment_pin4;
-    
-    long _origin_offset_steps = 0; // physical position that represents logical origin
 
+    /// @brief physical position that represents logical origin
+    long _origin_offset_steps = 0;
+
+    /// @brief the position to which we want to move
     long _target_position = 0;
 
     /// @brief Is the motor moving right now
     bool _moving;
 
+    /// @brief Logical coordinates are inverted vs. motor direction and referenced to originOffsetSteps
     long _logical_position();
 
+    /// @brief Convert desired logical position to physical steps for AccelStepper
     long _logical_to_physical(long logicalSteps);
 public:
+    /// @brief if you already have an attached stepper
+    /// @param stepper already attached stepper
     Stepper_controller(AccelStepper stepper);
+    /// @brief attaches the stepper to pins
     Stepper_controller(int pin1, int pin2, int pin3, int pin4);
+    /// @brief to initialize later. Note that as of writing this (10.03.2026, 20:08), this class does not check whether at any point the stepper is attached.
     Stepper_controller();
     ~Stepper_controller();
 
+    /// @brief attach the stepper to the given pins
     void attach(int pin1, int pin2, int pin3, int pin4);
 
+    /// @brief moves the stepper to the given position from origin
+    /// @param position position from origin
     void move_to_position(int position);
 
+    /// @brief moves the stepper the given amount from current position
+    /// @param amount the amount by which to move the stepper
     void move_by_amount(int amount);
 
-    void move_to_center();
+    /// @brief sets the current point as origin
+    void set_origin();
 
+    /// @brief simple test routine. This takes full control over the microcontroller as it executes
     void test_routine();
 
     /// @brief takes string input from the serial port, and moves the stepper accordingly
@@ -74,6 +87,10 @@ public:
     /// @return true if the motor is currently moving, false otherwise
     bool moving() { return _moving; };
 
+    /// @brief moves the stepper a single step. Should be used in the main loop of the micro controller
     void handle_movement();
+
+    /// @brief takes control of the micro controller until it reaches the desired position.
+    void priority_move();
 };
 
